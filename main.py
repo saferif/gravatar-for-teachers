619a61f3c50fed63c762da2467dd6dc03bc385bc
Insaf Latypov <saferif@gmail.com>
2015-07-06 22:59:17 +0300

    Current avatar added to dashboard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from handlers import web, api
 import secrets
 from webapp2 import uri_for, Route, WSGIApplication
 from webapp2_extras import jinja2
+from utils import md5
 
 config = {
     'webapp2_extras.sessions': {
@@ -25,5 +26,5 @@ application = WSGIApplication([
 ], debug=True, config=config)
 
 jinja2.set_jinja2(jinja2.Jinja2(application, {
-    'globals': {'uri_for': uri_for, 'site_name': 'My Gravatar'}
+    'globals': {'uri_for': uri_for, 'md5': md5, 'site_name': 'My Gravatar'}
 }), app=application)
